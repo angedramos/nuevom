54e4cbf5a8a5dd1ac78e7f79d444096faf189a35
Angel <angedramos@gmail.com>
2021-03-31 10:10:18 -0500

    Se cambió el saludo nuevamente y se cambió la propiedad

--- src/main/java/co/edu/eafit/protobootapp/ProtobootappApplication.java
+++ src/main/java/co/edu/eafit/protobootapp/ProtobootappApplication.java
@@ -21,7 +21,7 @@ public class ProtobootappApplication {
     //Se ejecuta con http://localhost:8080/
     @GetMapping("/")
     public String index() {
-        return String.format("Bienvenido! Esta aplicación ayuda a elear por potencias tipo calculadora.");
+        return String.format("Hola que tal, ¿Cómo les va?.");
     }    
 
 //Se ejecuta con http://localhost:8080/hola o http://localhost:8080/hola?nombre=John
